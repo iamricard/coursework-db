c9168263fb9afec14fa51ecf9fe5c737250648b5
Ricard Solé <ricardsolecasas@gmail.com>
2017-06-11 20:03:06 +0100

    adds SQL queries for views

---
+++ src/sql/views.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE VIEW bb AS
+SELECT bnfcodesub FROM chemicals
+WHERE chemicalname
+REGEXP 'atenolol|bisoprolol|carvedilol|metoprolol|nebivolol|propranolol';
+
+CREATE OR REPLACE VIEW bbprescriptions AS
+SELECT practiceid, quantity
+FROM prescriptions
+INNER JOIN bb
+ON prescriptions.bnfcode
+LIKE CONCAT(bb.bnfcodesub, '%');
+
+CREATE OR REPLACE VIEW bbpgp AS
+SELECT SUM(quantity) AS total, practiceid, practices.practicename
+FROM bbprescriptions
+LEFT JOIN practices
+ON bbprescriptions.practiceid = practices.id
+GROUP BY practiceid;
