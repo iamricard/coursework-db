aeeadf6d5dfd30a055329554e018b9527a120fae
Ricard Solé <ricardsolecasas@gmail.com>
2017-06-12 13:56:15 +0100

    finished exercise D

--- src/sql/01-views.sql
+++ src/sql/01-views.sql
@@ -1,35 +1,44 @@
 USE prescriptionsdb;
 
 CREATE OR REPLACE VIEW bb AS
-SELECT bnfcodesub FROM chemicals
-WHERE chemicalname
-REGEXP 'atenolol|bisoprolol|carvedilol|metoprolol|nebivolol|propranolol';
+SELECT bnfcodesub
+FROM chemicals
+WHERE chemicalname REGEXP 'atenolol|bisoprolol|carvedilol|metoprolol|nebivolol|propranolol';
+
 
 CREATE OR REPLACE VIEW bbprescriptions AS
-SELECT practiceid, quantity
+SELECT practiceid,
+       quantity
 FROM prescriptions
-INNER JOIN bb
-ON prescriptions.bnfcode
-LIKE CONCAT(bb.bnfcodesub, '%');
+INNER JOIN bb ON prescriptions.bnfcode LIKE CONCAT(bb.bnfcodesub, '%');
+
 
 CREATE OR REPLACE VIEW bbpgp AS
-SELECT SUM(quantity) AS total, practiceid, practices.practicename
+SELECT SUM(quantity) AS total,
+       practiceid,
+       practices.practicename
 FROM bbprescriptions
-LEFT JOIN practices
-ON bbprescriptions.practiceid = practices.id
+LEFT JOIN practices ON bbprescriptions.practiceid = practices.id
 GROUP BY practiceid;
 
+
 CREATE OR REPLACE VIEW ppgp AS
-SELECT COUNT(*) AS prescriptioncount, practiceid
+SELECT COUNT(*) AS prescriptioncount,
+       practiceid
 FROM prescriptions
 GROUP BY practiceid
 ORDER BY prescriptioncount DESC;
 
+
 CREATE OR REPLACE VIEW ppc AS
-SELECT
-  COUNT(*) AS dispensedamount,
-  SUBSTRING(bnfcode, 1, 9) AS bnfcodesub
-FROM
-  prescriptions
-GROUP BY
-  SUBSTRING(bnfcode, 1, 9);
+SELECT COUNT(*) AS dispensedamount,
+       SUBSTRING(bnfcode, 1, 9) AS bnfcodesub
+FROM prescriptions
+GROUP BY SUBSTRING(bnfcode, 1, 9);
+
+
+CREATE OR REPLACE VIEW spentpergp AS
+SELECT SUM(actualcost) AS spent,
+       practiceid
+FROM prescriptions
+GROUP BY practiceid;
--- src/sql/02-queries.sql
+++ src/sql/02-queries.sql
@@ -3,29 +3,19 @@ USE prescriptionsdb;
 -- a) How many practices and registered patients are there in the N17 postcode
 --    Area?
 
-SELECT
-  COUNT(*) AS 'practices in n17',
-  SUM(patientcount) AS 'patients in n17'
-FROM
-  practices
-LEFT JOIN
-  gppatients
-ON
-  gppatients.practiceid = practices.id
-WHERE
-  postcode
-LIKE
-  'N17%';
+SELECT COUNT(*) AS 'practices in n17',
+       SUM(patientcount) AS 'patients in n17'
+FROM practices
+LEFT JOIN gppatients ON gppatients.practiceid = practices.id
+WHERE postcode LIKE 'N17%';
 
 -- +------------------+-----------------+
 -- | practices in n17 | patients in n17 |
 -- +------------------+-----------------+
 -- |                7 | 49358           |
 -- +------------------+-----------------+
-
 -- b) Which practice prescribed the most beta blockers per registered patients
 --    in total over the two month period?
-
 -- Common beta-blockers include:
 -- atenolol
 -- bisoprolol
@@ -34,18 +24,12 @@ LIKE
 -- nebivolol
 -- propranolol
 
-SELECT
-  bbpgp.practiceid AS 'practice id',
-  bbpgp.total / gppatients.patientcount AS betablockersperpatient,
-  bbpgp.practicename AS 'practice name'
-FROM
-  bbpgp
-INNER JOIN
-  gppatients
-ON
-  gppatients.practiceid = bbpgp.practiceid
-ORDER BY
-  betablockersperpatient DESC
+SELECT bbpgp.practiceid AS 'practice id',
+       bbpgp.total / gppatients.patientcount AS betablockersperpatient,
+       bbpgp.practicename AS 'practice name'
+FROM bbpgp
+INNER JOIN gppatients ON gppatients.practiceid = bbpgp.practiceid
+ORDER BY betablockersperpatient DESC
 LIMIT 1;
 
 -- +-------------+------------------------+------------------------+
@@ -53,20 +37,13 @@ LIMIT 1;
 -- +-------------+------------------------+------------------------+
 -- | G82651      | 161.0000               | BURRSWOOD NURSING HOME |
 -- +-------------+------------------------+------------------------+
-
 -- c) Which was the most prescribed medication across all practices?
 
-SELECT
-  chemicalname,
-  dispensedamount
-FROM
-  chemicals
-INNER JOIN
-  ppc
-ON
-  ppc.bnfcodesub = chemicals.bnfcodesub
-ORDER BY
-  dispensedamount DESC
+SELECT chemicalname,
+       dispensedamount
+FROM chemicals
+INNER JOIN ppc ON ppc.bnfcodesub = chemicals.bnfcodesub
+ORDER BY dispensedamount DESC
 LIMIT 1;
 
 -- +----------------+-----------------+
@@ -74,8 +51,35 @@ LIMIT 1;
 -- +----------------+-----------------+
 -- | Colecalciferol |          280495 |
 -- +----------------+-----------------+
-
 -- d) Which practice spent the most and the least per patient?
+
+SELECT practicename,
+       spent
+FROM spentpergp
+INNER JOIN practices ON id = practiceid
+ORDER BY spent DESC
+LIMIT 1;
+
+
+SELECT practicename,
+       spent
+FROM spentpergp
+INNER JOIN practices ON id = practiceid
+ORDER BY spent ASC
+LIMIT 1;
+
+-- BIGGEST SPENDER
+-- +------------------------------+--------------------+
+-- | practicename                 | spent              |
+-- +------------------------------+--------------------+
+-- | MIDLANDS MEDICAL PARTNERSHIP | 1638640.1302093118 |
+-- +------------------------------+--------------------+
+-- CHEAPEST PEOPLE
+-- +------------------------------+---------------------+
+-- | practicename                 | spent               |
+-- +------------------------------+---------------------+
+-- | CRI BURY RECOVERY SERVICES   | 0.17000000178813934 |
+-- +------------------------------+---------------------+
 
 -- e) What was the difference in selective serotonin reuptake inhibitor
 --    prescriptions between January and February?
